4db3bf3a57cafa66b7c503dc43d4071f7c811975
Lukksky <lucassilvamatos94@gmail.com>
2025-02-14 23:00:15 -0300

    learning: 03-Persistindo dados mongoDB install and dependencies

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 import express from "express";
+import conectaNaDataBase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDataBase();
+
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso");
+})
 
 const app = express();
 app.use(express.json())
-
-const livros = [
-    {
-        id: 1,
-        titulo: "Código limpo"
-    },
-    {
-        id: 2,
-        titulo: "A metamorfose - Franz Kafka"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js")
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros)
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
@@ -51,3 +48,5 @@ app.delete("/livros/:id", (req, res) => {
 })
 
 export default app;
+
+
