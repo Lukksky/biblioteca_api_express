b7f55ea63df4a5085dac5219e2de70c266c505d7
Lukksky <lucassilvamatos94@gmail.com>
2025-02-18 22:53:57 -0300

    learning: create routes and configure Controllers MVC based

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,26 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+
+        } catch (err) {
+            res.status(500).json({ message: `${err.message} - falha na requisição` });
+        }
+        
+    };
+
+    static async listarLivroPorId (req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+
+        } catch (err) {
+            res.status(500).json({ message: `${err.message} - falha na requisição do livro` });
+        }
+        
     };
 
     static async cadastrarLivro (req, res) {
@@ -17,9 +35,37 @@ class LivroController {
 
         }
         
-    }
+    };
+
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "livro atualizado" });
+
+        } catch (err) {
+            res.status(500).json({ message: `${err.message} - falha na atualização do livro` });
+        }
+        
+    };
+
+    static async deletarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id, req.body);
+            res.status(200).json({ message: "livro excluído com sucesso" });
+
+        } catch (err) {
+            res.status(500).json({ message: `${err.message} - falha na exclusão do livro` });
+        }
+        
+    };
+
+
 
 };
+
+
 
 
 export default LivroController;
